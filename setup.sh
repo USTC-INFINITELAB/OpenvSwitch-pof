6a1b7d7a7de29ca400e528b073b70991810bde6f
sqy <sqy0410@mail.ustc.edu.cn>
2017-05-17 09:39:54 +0800

    rollback

--- setup.sh
+++ setup.sh
@@ -7,6 +7,11 @@ cd $DPDK_DIR
 export DPDK_TARGET=x86_64-native-linuxapp-gcc
 export DPDK_BUILD=$DPDK_DIR/$DPDK_TARGET
 #rm -r x86_64-native-linuxapp-gcc
+if [ ! -d /home/sqy/dpdk-16.07/x86_64-native-linuxapp-gcc ]
+then
+make install T=$DPDK_TARGET DESTDIR=install
+fi
+
 #make install T=$DPDK_TARGET DESTDIR=install
 # For IVSHMEM, Set `export DPDK_TARGET=x86_64-ivshmem-linuxapp-gcc`
 
@@ -14,15 +19,21 @@ cd /home/sqy
 export OVS_DIR=/home/sqy/OpenvSwitch-pof
 cd $OVS_DIR
 # ./boot.sh
-./configure --with-dpdk=$DPDK_BUILD
+#./configure --with-dpdk=$DPDK_BUILD
+./configure CFLAGS="-g -O0" --with-dpdk=$DPDK_BUILD
 make -j24
 make install
 
 echo 'vm.nr_hugepages=2048' > /etc/sysctl.d/hugepages.conf
 sysctl -w vm.nr_hugepages=2048
 #grep HugePages_ /proc/meminfo   #获取大页信息
-mount -t hugetlbfs none /dev/hugepages
+if [ ! -d /dev/hugepages ]
+then
+mkdir /dev/hugepages
+fi
 
+mount -t hugetlbfs none /dev/hugepages
+#ifconfig eth2 down
 #    modprobe vfio-pci
 #   sudo /usr/bin/chmod a+x /dev/vfio
 #    sudo /usr/bin/chmod 0666 /dev/vfio/*
@@ -41,7 +52,8 @@ modprobe uio_pci_generic
 #  sudo ifconfig ${port[$i]} down
 #  sudo ./tools/dpdk-devbind.py --bind=uio_pci_generic ${port[$i]}
 #done
-./tools/dpdk-devbind.py --bind=uio_pci_generic 0000:07:00.1
+./tools/dpdk-devbind.py --bind=uio_pci_generic 0000:05:00.1
+./tools/dpdk-devbind.py --bind=uio_pci_generic 0000:05:00.0
 ./tools/dpdk-devbind.py --status
 echo "DPDK Environment Success"
 cd $OVS_DIR
@@ -72,7 +84,7 @@ ovs-appctl vlog/set ofproto:ANY:dbg
 ovs-vsctl add-br br0 -- set bridge br0 datapath_type=netdev
 ovs-vsctl set-controller br0 tcp:192.168.109.160:6633
 ovs-vsctl add-port br0 dpdk0 -- set Interface dpdk0 type=dpdk
-#ovs-vsctl add-port br0 dpdk1 -- set Interface dpdk1 type=dpdk
+ovs-vsctl add-port br0 dpdk1 -- set Interface dpdk1 type=dpdk
 #ovs-ofctl show br0
 sleep 1s
 #ovs-appctl -t ovs-vswitchd exit
--- unsetup.sh
+++ unsetup.sh
@@ -11,19 +11,21 @@ cd  /home/sqy/dpdk-16.07
 #for((i=0;i<n;i++));do
 #     ./tools/dpdk-devbind.py --bind=igb ${port[$i]}
 #done
+./tools/dpdk-devbind.py --bind=igb 0000:05:00.1
 ./tools/dpdk-devbind.py --bind=igb 0000:05:00.0
 ./tools/dpdk-devbind.py  --status
 cd /home/sqy/OpenvSwitch-pof
 
 #ovs-vsctl del-br br0
-#ovs-appctl -t ovsdb-server exit
-#ovs-appctl -t ovs-vswitchd exit
+ovs-appctl -t ovs-vswitchd exit
+ovs-appctl -t ovsdb-server exit
 
 #rmmod openvswitch
+sleep 1s
 killall ovsdb-server
 killall ovs-vswitchd
 rm /usr/local/etc/openvswitch/conf.db
-sleep 1s
+sleep 5s
 umount -t hugetlbfs none /dev/hugepages
 sleep 1s
 grep HugePages_ /proc/meminfo
